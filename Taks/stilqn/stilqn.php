e3cb6269145bc0a2853273f8a812ddf27b335cc3
Freelife05 <83334140+Freelife05@users.noreply.github.com>
2021-11-21 22:36:23 +0200

    Newhomeworkdone

--- Taks/stilqn/stilqn.php
+++ Taks/stilqn/stilqn.php
@@ -13,4 +13,23 @@ function hello(){
 }
 hi();
 hello();
+
+$age = 17;
+if($age<"18"){
+    echo 'Im sorry your not allowed';
+}else{
+    echo'Come right in';
+}
+$number = 18;
+if($number > 18){
+ echo 'mercedess e320 2011';
+} else{
+    echo 'mercedess e200 2010';
+}
+$name = 'Stilqn';
+if($name == 'Stilqn'){
+  echo 'Mr', "$name";
+}else{
+    echo 'Mrs', "$name";
+}
 ?>
