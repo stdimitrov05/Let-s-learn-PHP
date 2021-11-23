ee3b3f4d1d4ee3a43401009260fc4857d73e6c71
velikow <83334334+velikow@users.noreply.github.com>
2021-11-23 10:59:42 +0200

    domashna za maika tiiiiii OOOOOOOOOOOOOOOOOOOOOOO

--- velikow.php
+++ velikow.php
@@ -1,13 +1,8 @@
 <?php
-$car = "370z";
-switch ($car) {
-    case 1:
-        echo '350z';
-        break;
-    case 3:
-        echo 'Acura Nsx';
-        break;
-    default:
-        echo 'error';
-    }
+$name = 'Velikow';
+if($name == 'Velikow'){
+  echo 'Mr', "$name";
+}else{
+    echo 'Mrs', "$name";
+}
 ?>
